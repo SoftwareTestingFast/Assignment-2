b885d8f0847c901ab57c9c2670c46017ae0134ce
MuskanZehra6 <113913290+MuskanZehra6@users.noreply.github.com>
2022-11-06 19:07:03 +0500

    Delete testing directory

--- testing/features/f1.feature
+++
@@ -1,4 +0,0 @@
-Feature: Visit website
-Background: Goto website
-  Given I open the React Store Front
-  Then the title is "React Storefront"
--- testing/features/step_definitions/f1.js
+++
@@ -1,9 +0,0 @@
-const { Given, Then, When, Before } = require('@cucumber/cucumber');
-
-Given(/^I open the React Store Front$/, function () {
-  return browser.navigateTo('http://localhost:3000');
-});
-
-Then(/^the title is "([^"]*)"$/, function (title) {
-  return browser.assert.titleEquals(title);
-});
